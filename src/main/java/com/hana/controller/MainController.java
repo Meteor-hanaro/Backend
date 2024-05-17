8b3d84ba60945cb117a0ce9094ba2f246ec7a210
tkdalsss <lsm991229@naver.com>
2024-05-17 18:41:42 +0900

    feat: initial setting

---
+++ src/main/java/com/hana/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.hana.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("")
+    public String main() {
+        return "hello meteor";
+    }
+}
