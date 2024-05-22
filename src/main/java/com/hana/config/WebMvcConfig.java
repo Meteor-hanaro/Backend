aa1a8dc9f656d34b62f5d5cf4352fb46290dd6be
tkdalsss <lsm991229@naver.com>
2024-05-22 09:34:20 +0900

    feat: cors proxy setting

---
+++ src/main/java/com/hana/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.hana.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "PUT", "POST", "DELETE");
+    }
+}
--- src/main/java/com/hana/controller/MainController.java
+++ src/main/java/com/hana/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @RequestMapping("")
+    @RequestMapping("/api")
     public String main() {
         return "hello meteor";
     }
