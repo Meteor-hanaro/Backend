24d53e23ac8d2aa5f13c898417f6b56d0425ebfb
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-27 17:42:08 +0900

    Feat: fundService test code

---
+++ src/test/java/com/hana/fund/SelectTests.java
@@ -0,0 +1,29 @@
+package com.hana.fund;
+
+import com.hana.app.data.entity.fund.Fund;
+import com.hana.app.service.FundService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@Slf4j
+@SpringBootTest
+public class SelectTests {
+
+    @Autowired
+    FundService fundService;
+
+    @Test
+    void contextLoads() {
+        List<Fund> x = fundService.getAll();
+        for (Fund fund : x) {
+            log.info(fund.getName());
+            log.info(fund.getRiskType().toString());
+        }
+    }
+
+}
