efc82f449eb8edfb582ab4ace02ad340e88ca24a
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-27 11:06:18 +0900

    Feat: Init base service frame

---
+++ src/main/java/com/hana/app/frame/BaseService.java
@@ -0,0 +1,21 @@
+package com.hana.app.frame;
+
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseService<K, V> {
+    @Transactional
+    public V insert(V v);
+
+    @Transactional
+    public Boolean delete(K k);
+
+    @Transactional
+    public V update(V v);
+
+    public Optional<V> get(K k);
+
+    public List<V> getAll();
+}
