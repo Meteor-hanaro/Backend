b4fa12f578a6f0f6f18610d96c0590e1d88d49fe
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-27 19:25:17 +0900

    Feat: fund controller returns fund list

--- src/main/java/com/hana/controller/FundController.java
+++ src/main/java/com/hana/controller/FundController.java
@@ -1,9 +1,29 @@
 package com.hana.controller;
 
+import com.hana.app.service.fund.FundSecurityService;
+import com.hana.app.service.fund.FundService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/fund")
+@RequiredArgsConstructor
 public class FundController {
+    final FundService fundService;
+    final FundSecurityService fundSecurityService;
+
+    @RequestMapping("/get")
+    public Object getFund() {
+        return fundService.getAll();
+    }
+
+    @RequestMapping("/securities/get")
+    public Object getFundSecurities(@RequestParam("id") Long id) {
+        Long relatedFundId = fundService.get(id).get().getId();
+        return fundSecurityService.get(relatedFundId);
+    }
+
 }
