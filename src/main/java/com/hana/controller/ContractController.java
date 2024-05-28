8c8cd6b6ea2e36f01116c9c67cb014fe4adb0f1f
jiyeon <sjy6736@naver.com>
2024-05-28 19:07:13 +0900

    feat: pdf 업로드용 api 구현

--- src/main/java/com/hana/controller/ContractController.java
+++ src/main/java/com/hana/controller/ContractController.java
@@ -1,9 +1,27 @@
 package com.hana.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import com.hana.external.aws.S3Service;
+import com.hana.response.MeteorResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
 @RequestMapping("/api/contract")
+@RequiredArgsConstructor
 public class ContractController {
+
+	private final S3Service s3Service;
+
+	@PostMapping("/upload")
+	public MeteorResponse.MeteorSuccessResponse<String> uploadPdf(@RequestParam("file") MultipartFile file) {
+		String fileUrl = s3Service.uploadPdf(file, "contract");
+		return MeteorResponse.success(fileUrl);
+	}
+
 }
