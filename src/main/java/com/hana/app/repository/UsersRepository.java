4828e9501f412d7aafcfe126f75dc7bdc9f62a3d
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-30 09:26:51 +0900

    Fix: Modification of user to users entity

--- src/main/java/com/hana/app/repository/UsersRepository.java
+++ src/main/java/com/hana/app/repository/UsersRepository.java
@@ -1,9 +1,9 @@
 package com.hana.app.repository;
 
-import com.hana.app.data.entity.User;
+import com.hana.app.data.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 }
