212044e3d605a38c42ee1f251d6044d13f3b0f56
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-30 15:19:29 +0900

    Feat: Get value of fund by date and fund id

--- src/main/java/com/hana/app/repository/security/SecurityPriceRepository.java
+++ src/main/java/com/hana/app/repository/security/SecurityPriceRepository.java
@@ -4,6 +4,9 @@ import com.hana.app.data.entity.security.SecurityPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SecurityPriceRepository extends JpaRepository<SecurityPrice, Long> {
+public interface SecurityPriceRepository extends JpaRepository<SecurityPrice, String> {
+    List<SecurityPrice> findBySecurityId(String securityId);
 }
--- src/main/java/com/hana/app/service/fund/FundService.java
+++ src/main/java/com/hana/app/service/fund/FundService.java
@@ -1,11 +1,16 @@
 package com.hana.app.service.fund;
 
 import com.hana.app.data.entity.fund.Fund;
+import com.hana.app.data.entity.fund.FundSecurity;
 import com.hana.app.frame.BaseService;
 import com.hana.app.repository.fund.FundRepository;
+import com.hana.app.repository.fund.FundSecurityRepository;
+import com.hana.app.repository.security.SecurityPriceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +19,8 @@ import java.util.Optional;
 public class FundService implements BaseService<Long, Fund> {
 
     final FundRepository fundRepository;
+    final FundSecurityRepository fundSecurityRepository;
+    final SecurityPriceRepository securityPriceRepository;
 
     @Override
     public Fund insert(Fund fund) {
@@ -42,4 +49,15 @@ public class FundService implements BaseService<Long, Fund> {
     public List<Fund> getAll() {
         return fundRepository.findAll();
     }
+
+    public Long getValueByDate(Long fundId, LocalDateTime currentDate) {
+        List<FundSecurity> x = fundSecurityRepository.findFundSecuritiesByFund_IdOrderByFundSecurityPercentageDesc(fundId);
+        List<Long> fundValues = new ArrayList<>();
+        x.forEach((xx) -> {
+            String security_id = xx.getSecurity().getId();
+            fundValues.add(securityPriceRepository.findSecurityPriceByTradeDateAndSecurityId(currentDate, security_id));
+        });
+        return fundValues.stream().mapToLong(Long::longValue).sum();
+    }
+
 }
