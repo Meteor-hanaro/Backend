da129d74dabf71164d905de909305f0d35bf98dc
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-30 11:14:20 +0900

    Feat: Addition of fund id in portfolio item dto

--- src/main/java/com/hana/dto/response/PortfolioItemDto.java
+++ src/main/java/com/hana/dto/response/PortfolioItemDto.java
@@ -3,14 +3,14 @@ package com.hana.dto.response;
 import com.hana.app.data.entity.portfolio.PortfolioItem;
 import lombok.Getter;
 
-import javax.sound.sampled.Port;
-
 @Getter
 public class PortfolioItemDto {
+    private final Long fundId;
     private final Long startAmount;
     private final Long evaluationAmount;
 
     public PortfolioItemDto(PortfolioItem portfolioItem) {
+        this.fundId = portfolioItem.getFund().getId();
         this.startAmount = portfolioItem.getStartAmount();
         this.evaluationAmount = portfolioItem.getEvaluationAmount();
     }
