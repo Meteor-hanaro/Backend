6fead2f1903a02a5cc200c569c17a8b112c4f5ed
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-31 17:41:26 +0900

    Feat: Init vip repository

---
+++ src/main/java/com/hana/app/repository/VipRepository.java
@@ -0,0 +1,7 @@
+package com.hana.app.repository;
+
+import com.hana.app.data.entity.VIP;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VipRepository extends JpaRepository<VIP, Long> {
+}
