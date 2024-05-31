a69bda1d409d8ee0aa364fd1ee7b3432993b2fd5
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-31 10:49:56 +0900

    Feat: getSecurityQuantity

--- src/main/java/com/hana/app/service/PortfolioItemService.java
+++ src/main/java/com/hana/app/service/PortfolioItemService.java
@@ -3,36 +3,50 @@ package com.hana.app.service;
 import com.hana.app.data.entity.fund.Fund;
 import com.hana.app.data.entity.fund.FundSecurity;
 import com.hana.app.data.entity.portfolio.PortfolioItem;
+import com.hana.app.repository.fund.FundRepository;
 import com.hana.app.repository.fund.FundSecurityRepository;
 import com.hana.app.repository.security.SecurityPriceRepository;
 import com.hana.dto.response.CodeQuantityDto;
 import com.hana.dto.response.PurchaseCompositionDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PortfolioItemService {
 
     final FundSecurityRepository fundSecurityRepository;
     final SecurityPriceRepository securityPriceRepository;
+    final FundRepository fundRepository;
 
-//    가입 당시 구조 확보
+    public String dummy() {
+        return "HEHEH";
+    }
+    //    가입 당시 구조 확보
     public PurchaseCompositionDto getSecurityQuantity(PortfolioItem portfolioItem, Long inputValue) {
-        List<FundSecurity> fundSecurities = fundSecurityRepository.findFundSecuritiesByFund_IdOrderByFundSecurityPercentageDesc(portfolioItem.getFund().getId());
-        Long leftover = 0L;
-        List<CodeQuantityDto> codeQuantityDtoList = new ArrayList<>();
-        for (FundSecurity fs : fundSecurities) {
-            long inputMoney = inputValue * fs.getFundSecurityPercentage() / 100;
-            long value_back_then = securityPriceRepository.findSecurityPriceByTradeDateAndSecurityId(portfolioItem.getCreatedAt(), fs.getSecurity().getId()).getTradePrice();
-            int purchased_quantity = (int) Math.floor((double) inputMoney / value_back_then);
-            leftover += inputMoney - purchased_quantity * value_back_then;
-            codeQuantityDtoList.add(CodeQuantityDto.from(fs.getSecurity().getId(), purchased_quantity));
-        }
+        Optional<Fund> fund_connected_with_item = fundRepository.findById(portfolioItem.getFund().getId());
+        LocalDateTime purchase_date = portfolioItem.getCreatedAt();
+        List<CodeQuantityDto> codeQuantityDtos = new ArrayList<>();
+        if (fund_connected_with_item.isPresent()) {
+            Fund fund = fund_connected_with_item.get();
+//            펀드의 구조
+            List<FundSecurity> fundSecurities = fundSecurityRepository.findFundSecuritiesByFund_IdOrderByFundSecurityPercentageDesc(fund.getId());
+            fundSecurities.forEach(security -> {
+//                펀드 구입 시점의 security의 가격
+                Long init_price = securityPriceRepository.findSecurityPriceByTradeDateAndSecurityId(purchase_date, security.getSecurity().getId()).getTradePrice();
+                Long init_quantity = inputValue * (security.getFundSecurityPercentage() / 100) / init_price;
+                codeQuantityDtos.add(CodeQuantityDto.from(security.getSecurity().getId(), init_quantity));
+            });
 
-        return PurchaseCompositionDto.from(codeQuantityDtoList, leftover);
+            return PurchaseCompositionDto.from(codeQuantityDtos);
+        }
+        return null;
     }
 }
