84a430fe997a86e41e587c7007fad1574e379f22
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-31 17:34:34 +0900

    Feat: findSecurityPriceByTradeDateAndSecurityId

--- src/main/java/com/hana/app/repository/security/SecurityPriceRepository.java
+++ src/main/java/com/hana/app/repository/security/SecurityPriceRepository.java
@@ -4,9 +4,12 @@ import com.hana.app.data.entity.security.SecurityPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface SecurityPriceRepository extends JpaRepository<SecurityPrice, String> {
     List<SecurityPrice> findBySecurityId(String securityId);
+
+    SecurityPrice findSecurityPriceByTradeDateAndSecurityId(LocalDateTime tradeDate, String securityId);
 }
