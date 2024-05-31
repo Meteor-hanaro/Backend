a391a93381ff86831bba3bd19b3e8566ab03112a
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-31 17:27:53 +0900

    Feat: Addition of item id and fund name in portfolio item dto

--- src/main/java/com/hana/dto/response/PortfolioItemDto.java
+++ src/main/java/com/hana/dto/response/PortfolioItemDto.java
@@ -3,18 +3,24 @@ package com.hana.dto.response;
 import com.hana.app.data.entity.portfolio.PortfolioItem;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PortfolioItemDto {
+    private final Long itemId;
     private final Long fundId;
     private final String fundName;
     private final Long startAmount;
     private final Long evaluationAmount;
+    private final LocalDateTime createdAt;
 
     public PortfolioItemDto(PortfolioItem portfolioItem) {
+        this.itemId = portfolioItem.getId();
         this.fundId = portfolioItem.getFund().getId();
         this.fundName = portfolioItem.getFund().getName();
         this.startAmount = portfolioItem.getStartAmount();
         this.evaluationAmount = portfolioItem.getEvaluationAmount();
+        this.createdAt = portfolioItem.getCreatedAt();
     }
 
     public static PortfolioItemDto from(PortfolioItem portfolioItem) {
