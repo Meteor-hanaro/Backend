b3134aa060b7d1d86e70438fa79e91766ea1e1b5
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-31 17:43:55 +0900

    Feat: get vip by vip id in vip service

---
+++ src/main/java/com/hana/app/service/VipService.java
@@ -0,0 +1,18 @@
+package com.hana.app.service;
+
+import com.hana.app.data.entity.VIP;
+import com.hana.app.repository.VipRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class VipService {
+    final VipRepository vipRepository;
+
+    public VIP getVipByVipId(Long vipId) {
+        return vipRepository.findById(vipId).get();
+    }
+}
