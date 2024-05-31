73e86f765d52cf0cefcf10801a4c09825f55f0da
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-31 17:31:57 +0900

    Feat: item value controller for portfolio table view

--- src/main/java/com/hana/controller/PortfolioController.java
+++ src/main/java/com/hana/controller/PortfolioController.java
@@ -1,7 +1,12 @@
 package com.hana.controller;
 
+import com.hana.app.data.entity.portfolio.PortfolioItem;
+import com.hana.app.service.PortfolioItemService;
 import com.hana.app.service.PortfolioService;
+import com.hana.app.service.fund.FundService;
 import com.hana.dto.response.PortfolioDto;
+import com.hana.dto.response.PortfolioItemDto;
+import com.hana.dto.response.PortfolioItemResponseDto;
 import com.hana.exception.MeteorException;
 import com.hana.exception.NotFoundException;
 import com.hana.response.ErrorType;
@@ -10,12 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/portfolio")
 @RequiredArgsConstructor
 public class PortfolioController {
 
     final PortfolioService portfolioService;
+    final PortfolioItemService portfolioItemService;
+    private final FundService fundService;
 
     @RequestMapping("/extract")
     public PortfolioDto getPortfolio(@RequestParam("vipId") Long vipId) {
@@ -28,5 +39,24 @@ public class PortfolioController {
         }
 
         return portfolioDto;
+    }
+
+    @RequestMapping("/itemValue")
+    public List<PortfolioItemResponseDto> getPortfolioItemValue(@RequestParam("vipId") Long vipId) {
+        List<PortfolioItemDto> portfolioItems = portfolioService.getPortfolioByVipId(vipId).getPortfolioItems();
+
+        List<PortfolioItemResponseDto> portfolioItemResponseDtos = new ArrayList<>();
+        
+        portfolioItems.forEach((x) -> {
+
+            String fundName = x.getFundName();
+            Long startAmount = x.getStartAmount();
+            LocalDateTime startDate = x.getCreatedAt().withHour(0).withMinute(0).withSecond(0).withNano(0);
+            Long evaluationAmount = portfolioItemService.getCurrentValue(x.getItemId());
+            portfolioItemResponseDtos.add(PortfolioItemResponseDto.from(fundName, startAmount, startDate, evaluationAmount));
+        });
+
+
+        return portfolioItemResponseDtos;
     }
 }
