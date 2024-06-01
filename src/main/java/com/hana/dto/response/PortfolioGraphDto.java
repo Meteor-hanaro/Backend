1b2fc9cd18f169ecc92d9e8dfc0a793089068d0e
Kwak-JunYoung <klop9090@g.skku.edu>
2024-06-01 17:47:25 +0900

    Feat: Addition of serial time data in portfolio graph dto

--- src/main/java/com/hana/dto/response/PortfolioGraphDto.java
+++ src/main/java/com/hana/dto/response/PortfolioGraphDto.java
@@ -3,15 +3,17 @@ package com.hana.dto.response;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class PortfolioGraphDto {
     private final String securityCode;
+    private final List<LocalDateTime> serialTime;
     private final List<Long> serialValue;
 
-    public static PortfolioGraphDto from(String securityCode, List<Long> serialValue) {
-        return new PortfolioGraphDto(securityCode, serialValue);
+    public static PortfolioGraphDto from(String securityCode, List<LocalDateTime> serialTime, List<Long> serialValue) {
+        return new PortfolioGraphDto(securityCode, serialTime, serialValue);
     }
 }
