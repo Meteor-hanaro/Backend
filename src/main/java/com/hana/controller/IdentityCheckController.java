d5828ccf0e0f8d8b998740dfa315edbe34e8fe83
juhye <rlawngp124@gmail.com>
2024-06-03 09:19:11 +0900

    feat: auth확인

--- src/main/java/com/hana/controller/IdentityCheckController.java
+++ src/main/java/com/hana/controller/IdentityCheckController.java
@@ -4,11 +4,14 @@ import com.hana.util.OCRUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Map;
 
 @Slf4j
@@ -19,9 +22,12 @@ public class IdentityCheckController {
     private String ocrkey;
     @Value("${apikey.ocrurl}")
     private String ocrurl;
+
     @PostMapping("/ocr")
     public String ocr(@RequestParam("image") MultipartFile image) {
         log.info("==========checking image========="+image.getOriginalFilename());
+//        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+//        autthentication.getName();
         if(image==null){
             log.info("image is null check your code");
             return "image is null";
@@ -37,6 +43,7 @@ public class IdentityCheckController {
 
             Map<String,String> map = OCRUtil.getData(jsonObject);
             map.values().forEach(txt->{log.info(txt);});
+
 
             return jsonObject.toJSONString();
 
