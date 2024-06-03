929e2341f876532d16e40263473c9416c343c6b3
jiyeon <sjy6736@naver.com>
2024-06-03 15:32:26 +0900

    feat: 가입할 펀드들의 관련 계약서 가져오기

--- src/main/java/com/hana/app/repository/fund/FundRepository.java
+++ src/main/java/com/hana/app/repository/fund/FundRepository.java
@@ -1,9 +1,13 @@
 package com.hana.app.repository.fund;
 
+import java.util.List;
+
 import com.hana.app.data.entity.fund.Fund;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FundRepository extends JpaRepository<Fund, Long> {
+
+	List<Fund> findByIdIn(List<Long> ids);
 }
