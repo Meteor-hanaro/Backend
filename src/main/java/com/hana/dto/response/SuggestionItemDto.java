20843a053de97341e9c1c5fcdf75daebc100933a
Kwak-JunYoung <klop9090@g.skku.edu>
2024-06-09 20:32:20 +0900

    Feat: Addition of suggestion id in suggestionItemDto

--- src/main/java/com/hana/dto/response/SuggestionItemDto.java
+++ src/main/java/com/hana/dto/response/SuggestionItemDto.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 @Getter
 public class SuggestionItemDto {
+    private final Long suggestionId;
     private final String suggestionName;
     private final List<SuggestionItemCompositionDto> suggestionItems;
 
     public SuggestionItemDto(Suggestion suggestion, List<SuggestionItemCompositionDto> suggestionItems) {
+        this.suggestionId = suggestion.getId();
         this.suggestionName = suggestion.getName();
         this.suggestionItems = suggestionItems;
     }
