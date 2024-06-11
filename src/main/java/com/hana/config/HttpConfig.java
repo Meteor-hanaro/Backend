b22d4982b7149515bd1b76ca2f43cb69761ecaf9
tkdalsss <lsm991229@naver.com>
2024-06-11 11:23:59 +0900

    feature/deploy setting

---
+++ src/main/java/com/hana/config/HttpConfig.java
@@ -0,0 +1,16 @@
+package com.hana.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class HttpConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.requiresChannel((channel) -> channel.anyRequest().requiresSecure());
+        return http.build();
+    }
+}
